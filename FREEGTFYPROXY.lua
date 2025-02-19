fcc9ec9417cc2d301bed38cb1e7ab23b3ca85a2f
iHkaz <140932825+ihkaz@users.noreply.github.com>
2025-02-19 13:03:27 +0700

    Update FREEGTFYPROXY.lua

--- FREEGTFYPROXY.lua
+++ FREEGTFYPROXY.lua
@@ -15,7 +15,7 @@ add_smalltext|https://dsc.gg/ihkaz|left|
 add_spacer|small|
 add_label_with_icon|small|What's New? PATCH : [`419/02/2025``]|left|6124|
 add_spacer|small|
-add_smalltext|[+] Commands : [/abso,/wd,/depo]
+add_smalltext|[+] Commands : [/abso,/wd,/depo]|left|
 add_spacer|small|
 add_smalltext|`2Creator`` : `1@pangerans|left|
 add_spacer|small|
